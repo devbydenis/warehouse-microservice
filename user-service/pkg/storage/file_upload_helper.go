ff71a697edb461194883fed1ea41851fb0a55dbd
devbydenis <hello.denisrahmadi@gmail.com>
2025-11-16 21:58:31 +0700

    fix: logical validate file size and file extension

--- user-service/pkg/storage/file_upload_helper.go
+++ user-service/pkg/storage/file_upload_helper.go
@@ -44,11 +44,11 @@ func (h *FileUploadHelper) UploadPhoto(ctx context.Context, file *multipart.File
 }
 
 func (h *FileUploadHelper) validateImageFile(file *multipart.FileHeader, maxSize int64) error {
-	if validateFileSize(file.Size, maxSize) {
+	if !validateFileSize(file.Size, maxSize) {
 		return fmt.Errorf("file size exceeds the maximum allowed size")
 	}
 
-	if validateFileExtension(getFileExtension(file.Filename), AllowedImageExtensions) {
+	if !validateFileExtension(getFileExtension(file.Filename), AllowedImageExtensions) {
 		return fmt.Errorf("invalid file extension")
 	}
 
